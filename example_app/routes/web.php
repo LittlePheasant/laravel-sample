7286c476c75b6fe63efe9cfaf852cfce008edb79
Kenneth <palencia.kenneth69@gmail.com>
2023-08-17 15:12:18 +0800

    simple to-do-list app

--- example_app/routes/web.php
+++ example_app/routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoListController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+//->name() = alias to be used in binding
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TodoListController::class, 'index']) ->name('home');
+Route::post('/save-task', [TodoListController::class, 'saveTask']) ->name('saveTask');
+Route::get('/delete-task/{id}', [TodoListController::class, 'deleteTask']) ->name('deleteTask');
+Route::get('/update-task/{id}', [TodoListController::class, 'updateTask']) ->name('updateTask');
+Route::post('/save-updated-task', [TodoListController::class, 'saveUpdatedTask']) ->name('saveUpdatedTask');
